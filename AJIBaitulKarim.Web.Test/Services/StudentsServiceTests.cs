684cdc1e0c8d8edb8f27a0b1ff208daaad9496b1
MD YASIR ARAFAT <ping@arafat.pro>
2021-11-17 16:36:21 +0600

    Initialization Unit Test Porject

---
+++ AJIBaitulKarim.Web.Test/Services/StudentsServiceTests.cs
@@ -0,0 +1,17 @@
+using Xunit;
+
+namespace AJIBaitulKarim.Web.Test
+{
+    public class StudentsServiceTests
+    {
+        [Fact]
+        public void Test1()
+        {
+            //given
+
+            //when
+
+            //then
+        }
+    }
+}
